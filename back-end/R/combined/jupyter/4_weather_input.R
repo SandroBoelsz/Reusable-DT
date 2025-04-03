4d39065d02dcb8520f1c26553cc09c09c1e85002
Sandro Boelsz <97670955+SandroBoelsz@users.noreply.github.com>
2025-04-03 11:50:58 +0200

    Add the rest of the experiment

--- back-end/R/combined/jupyter/4_weather_input.R
+++ back-end/R/combined/jupyter/4_weather_input.R
@@ -1,12 +1,11 @@
-# 2 Prepare locations and netlogo input
+# Three Create weather resources
 # ---
 # NaaVRE:
 #  cell:
 #   inputs:
-#    - isFileDownloadSuccessful: Integer
-#   outputs:
 #    - locations_output: List
-#    - netlogo_output: List
+#   outputs:
+#    - input_file: String
 #   params:
 #    - param_s3_region:
 #       type: String
@@ -50,102 +49,128 @@
 #    - param_model:
 #       type: String
 #       default_value: "Beehave_BeeMapp2015_Netlogo6version_PolygonAggregation.nlogo"
+#    - param_netlogo_jar_path:
+#       type: String
+#       default_value: "test"
 #   secrets:
 #    - secret_s3_access_id:
 #       type: String
 #    - secret_s3_secret_key:
 #       type: String
 #   dependencies:
-#    - name: readr
-#    - name: purrr
+#    - name: terra
 #    - name: dplyr
+#    - name: purrr
+#    - name: lubridate
+#    - name: raster
+#    - name: Rcpp
+#    - name: rdwd
+#    - name: stringr
 # ...
 
-library(readr)
-library(purrr)
+library(terra)
 library(dplyr)
+library(purrr)
+library(stringr)
+library(rdwd)
+library(lubridate)
 
-if (!isFileDownloadSuccessful) {
-  stop("File download failed! Stopping workflow.")
-} else {
-  message("All files downloaded successfully. Proceeding with input preparation.")
-}
+###### R - script to create input (resources and weather) files to run the BEEHAVE model
+# main contributor Anna Wendt, University of Freiburg
+# contributor of an earlier version of the WeatherDataInput() function Okan Özsoy
+# modifications have been done by Jürgen Groeneveld, Tomas Martinovic, Tuomas Rossi
+# the honeybee pDT has benefited from the work of the honeybee pDT team
 
-input_tif_path <- file.path(param_input_dir, param_map)
-input_lookup_path <- file.path(param_input_dir, param_lookup_table)
-input_locations_path <- file.path(param_input_dir, param_locations)
-input_parameters_path <- file.path(param_input_dir, param_parameters)
-input_simulation_path <- file.path(param_input_dir, param_simulation)
-
-if (!dir.exists(param_output_dir)) {
-  dir.create(param_output_dir)
-}
-
-# Load locations ----
-location <- 
-  read_delim(input_locations_path,
-             delim = ",",
-             col_types = list(
-               id = "i",
-               lat = "d",
-               lon = "d"
-  )) |>
-  slice(1)
-
-# Load parameters ----
-parameters <- 
-  read_csv(
-    file = input_parameters_path,
-    col_types = list(
-      Parameter = col_character(),
-      Value = col_double(),
-      `Default.Value` = col_skip()
-    )
-  ) |>
-  rbind(
-    data.frame(
-      Parameter = c("INPUT_FILE", "WeatherFile", "random-seed"),
-      Value = c(paste0("\"", param_input_dir, "locations", "/input.txt\""),
-                paste0("\"", param_input_dir, "locations", "/weather.txt\""),
-                sample(1:100000, 1))
-    )
-)
-
-parameters_list <- parameters$Value |>
-  map(~list(.x))
-names(parameters_list) <- parameters$Parameter
+# Function to create a weatherinput file for the beehave model
+weather_data_input <- function(bee_location,
+                               from_date = "2016-01-01",
+                               to_date = "2016-12-31") {
+  # transform input coordinates to degrees
+  TrachtnetConv <- project(bee_location, "epsg:4326")
+  Coordinates <- as.data.frame(crds(TrachtnetConv))
   
-# Load simulation data ----
-simulation_df <- read_csv(
-  file = input_simulation_path,
-  col_types = list(
-    sim_days = col_integer(),
-    start_day = col_character()
-  )
-)
-locations_output <- list(
-  id = location$id,
-  lat = location$lat,
-  lon = location$lon,
-  buffer_size = param_buffer,
-  sim_days = simulation_df$sim_days[1],
-  start_day = simulation_df$start_day[1],
-  location_path = file.path(param_input_dir, "locations"),
-  input_tif_path = input_tif_path,
-  nectar_pollen_lookup_path = input_lookup_path
-)
+  # Read the station data
+  WeatherStations <- nearbyStations(
+    Coordinates$y,
+    Coordinates$x,
+    radius = 50,
+    res = "daily", var = "kl", per = "historical", mindate = to_date
+  ) |>
+    # select only stations that started measuring before 2016
+    filter(von_datum < from_date) |>
+    # change urls to https
+    mutate(
+      url = map_chr(url,
+      function(x){str_replace_all(x, "ftp://", "https://")})) 
+  
+  # check through the stations for NA values in data
+  for (i in 1:nrow(WeatherStations)) {
+    weather_data <- dataDWD(WeatherStations$url[i], varnames = TRUE, quiet = TRUE) |>
+      select(MESS_DATUM,
+             SDK.Sonnenscheindauer,
+             TXK.Lufttemperatur_Max) |>
+      # mutate(MESS_DATUM = as_date(MESS_DATUM)) |>
+      filter(MESS_DATUM >= as.POSIXct(from_date, tz = "GMT"),
+             MESS_DATUM <= as.POSIXct(to_date, tz = "GMT")) 
+
+    # breaks when file with no NAs in SDK found
+    if (anyNA(weather_data$SDK.Sonnenscheindauer) == FALSE & length(weather_data$SDK.Sonnenscheindauer) > 0) break
     
-netlogo_output <- list(
-  outpath = file.path(
-    param_output_dir,
-    paste0("output_id", location$id, ".csv")
+    # if all stations contain NA values give warning
+    if (i == length(WeatherStations$Stations_id)) {
+      warning(paste("Final selected weather station includes NA values. No stations found without any NA within 50km distance. Station ID:", WeatherStations$Stations_id[i]))
+    }
+  }
+  
+  # Add station id and day number
+  weather_data <- weather_data |>
+    rename(Date = MESS_DATUM,
+           T_max = TXK.Lufttemperatur_Max,
+           Sun_hours = SDK.Sonnenscheindauer) |>
+    mutate(Station_id = WeatherStations$Stations_id[i],
+           Day = 1:n(),
+           .before = Date) |>
+    # Use only sun hours where max temperature is above 15 degrees celsium
+    mutate(Sun_hours = ifelse(T_max < 15, 0, Sun_hours))
+  
+  # create vector for BEEHAVE weather input with sunshine hours
+  weather_file <- paste("[", paste(weather_data$Sun_hours, collapse = " "), "]")
+  
+  return(list(weather_data, weather_file))
+}
+
+input_map <-
+  rast(locations_output$input_tif_path)
+
+bee_location <- vect(
+  data.frame(
+    id = locations_output$id,
+    lon = locations_output$lon,
+    lat = locations_output$lat
   ),
-  metrics = c(
-    "TotalIHbees + TotalForagers",
-    "(honeyEnergyStore / ( ENERGY_HONEY_per_g * 1000 ))",
-    "PollenStore_g"
+  geom = c("lon", "lat"),
+  crs = "EPSG:4326"
+) |>
+  project(input_map)
+
+# Create weather input for beehave and write file ----
+to_date <- locations_output$start_day |>
+  as.Date() + locations_output$sim_days
+
+WeatherOutput <- weather_data_input(bee_location,
+                                    from_date = locations_output$start_day,
+                                    to_date = to_date)
+
+write.table(
+  WeatherOutput[2],
+  paste0(
+    locations_output$location_path,
+    "/weather",
+    ".txt"
   ),
-  variables = parameters_list,
-  sim_days = simulation_df$sim_days[1],
-  start_day = simulation_df$start_day[1]
+  quote = F,
+  row.names = F,
+  col.names = F
 )
+
+weather_file <- paste0(locations_output$location_path, "/weather.txt")
