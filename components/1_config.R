3e1892186f534cc616e0e447e63b572e3caa51d5
Sandro Boelsz <97670955+SandroBoelsz@users.noreply.github.com>
2025-06-19 16:49:14 +0200

    new config

---
+++ components/1_config.R
@@ -0,0 +1,18 @@
+# Default Template Configuration
+param_input_dir <- "/tmp/data/input/" 
+param_output_dir <- "/tmp/data/output/" 
+param_buffer <- 3000L
+param_map <- "map.tif"
+param_map_aux <- "map.tif.aux.xml"
+param_lookup_table <- "lookup_table.csv"
+param_locations <- "locations.csv"
+param_parameters <- "parameters.csv"
+param_simulation <- "simulation.csv"
+param_model <- "Beehave_BeeMapp2015_Netlogo6version_PolygonAggregation.nlogo"
+
+param_s3_endpoint <- ""
+param_s3_bucket <- "naa-vre-user-data"
+param_s3_user_prefix <- ""
+param_s3_region <- "nl-uvalight"
+secret_s3_access_id <- ""
+secret_s3_secret_key <- ""
